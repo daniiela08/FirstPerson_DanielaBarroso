3ce7024abe080c72aca90b62818aadbf0b3f67d2
daniiela08 <danielabarrosoruiz@gmail.com>
2024-11-27 18:29:24 +0100

    AA

--- Assets/SCRIPTS/ArmaManual.cs
+++ Assets/SCRIPTS/ArmaManual.cs
@@ -6,6 +6,7 @@ public class ArmaManual : MonoBehaviour
 {
     [SerializeField] private ArmaSO myData;
     private Camera cam;
+    [SerializeField] private ParticleSystem particulas;
     void Start()
     {
         cam = Camera.main;
@@ -14,6 +15,7 @@ public class ArmaManual : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            particulas.Play();
             if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hitInfo, myData.DistanciaAtaque))
             {
                 if (hitInfo.transform.CompareTag("ParteEnemigo"))
